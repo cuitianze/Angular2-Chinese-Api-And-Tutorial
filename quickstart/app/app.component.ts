5701669ba0a1c22ae6555f557cde1d54ceae7d0a
cuitianze <cuitianze123@gmail.com>
2015-12-20 00:45:29 +0800

    quickstart

---
+++ quickstart/app/app.component.ts
@@ -0,0 +1,61 @@
+import {Component} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+
+console.log(HeroDetailComponent);
+
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>My first Angular 2 App</h1>
+  <h2>{{title}}</h2>
+  <ul class="heroes">
+  	<li *ngFor="#hero of heros" [class.selected]="hero === selectedHero" (click)='onSelect(hero)'>
+  		<span class="badge">{{hero.id}}</span> {{hero.name}}
+  	</li>
+  </ul>
+  {{selectedHero && selectedHero.name}}
+  <my-hero-detail [hero]='selectedHero'></my-hero-detail>
+  `,
+  styles:[`
+	  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+	  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+	  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+	  .heroes .badge {
+	    font-size: small;
+	    color: white;
+	    padding: 0.1em 0.7em;
+	    background-color: #369;
+	    line-height: 1em;
+	    position: relative;
+	    left: -1px;
+	    top: -1px;
+	  }
+	  .selected { background-color: #EEE; color: #369; }
+  `],
+  directives: [HeroDetailComponent]
+})
+
+export class AppComponent {
+	public title = 'CuiTianze';
+	public heros = HEROES;
+	public selectedHero: Hero;
+	onSelect(hero: Hero) {
+		this.selectedHero = hero;
+	}
+}
